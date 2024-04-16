0dc99086d9629c556c493d00dc91d46248ad13dd
Ambika-sys <61460001+Ambika-sys@users.noreply.github.com>
2024-04-16 14:05:06 +0200

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -16,8 +16,4 @@ variable "region" {
 resource "aws_instance" "public-instance" {
   ami           = "ami-0c101f26f147fa7fd"
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public-subnet.id
-  tags = {
-    Name = "public-instance"
-  }
 }
